4749bb394d0425f0c9fd1f35cec2e7254e9d80db
Seval Şentürk <sevalsenturk2017@gmail.com>
2022-08-23 16:40:59 +0300

    çalışma başlasın

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr-TR">
 <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Seval Şentürk">
+    <meta http-equiv="content-type" content="text/html">
+    <meta http-equiv="content-language"  content="tr">
     <title>BTK HTML Dersleri</title>
 </head>
 <body>
+
+
 
 
 </body>
